5dece139f90dc345f5f864b303b364ff229b3740
Michael de Silva <mike@stembolt.com>
2015-10-03 03:34:52 +0530

    Scoring project, done

--- koans/scoring_project.rb
+++ koans/scoring_project.rb
@@ -1,5 +1,23 @@
 # Scoring Project Code.
 
 def score(dice)
-  return 0 if dice.empty?
+  tally = 0
+
+  counts = Hash.new(0)
+  dice.each { |x| counts[x] += 1 }
+
+  (1..6).each do |i|
+    if counts[i] >= 3
+      i == 1 ? tally += 1000 : tally += 100 * i
+      counts[i] = [counts[i] - 3, 0].max
+    end
+
+    if i == 1
+      tally += 100 * counts[i]
+    elsif i == 5
+      tally += 50 * counts[i]
+    end
+  end
+
+  tally
 end
