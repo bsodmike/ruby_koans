a54d2cf36a4ab29b0a099474a541118c7cfcedf7
Michael de Silva <mike@stembolt.com>
2015-10-03 01:16:53 +0530

    Triangle project 2, done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a, b, c = [a, b, c].sort
+
+  raise TriangleError, "The smallest side must be positive in length" if a <= 0
+  raise TriangleError, "The sum of the two smallest sides must be greater than the largest side" if a + b <= c
+
   case
   when (a**3) == (a * b * c) # comparing side^3 when lengths are equal
     :equilateral
