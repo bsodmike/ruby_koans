f8744dc628c85dca6eab4766e2edf9dd3cbf4149
Michael de Silva <mike@stembolt.com>
2015-10-07 00:58:39 +0530

    Spec Player API for score increment/decrement & getter

--- koans/greed/spec/greed/player_spec.rb
+++ koans/greed/spec/greed/player_spec.rb
@@ -9,4 +9,21 @@ class Greed::PlayerSpec < Neo::Koan
   def test_can_get_players_name
     assert_equal "Foo", Player.new("Foo").name
   end
+
+  SpecPlayer = Player.new("Mike")
+
+  def test_can_get_player_score
+    assert_equal 0, SpecPlayer.score
+  end
+
+  def test_can_increment_player_score
+    assert_equal 200, SpecPlayer.increment_score(200)
+    assert_equal 350, SpecPlayer.increment_score(150)
+  end
+
+  def test_can_also_decrement_player_score
+    assert_equal 300, SpecPlayer.decrement_score(50)
+    assert_equal 0, SpecPlayer.decrement_score(350) # Prevents a negative score
+  end
+
 end
