366645e36cb4d9dad3505ce6f29ed28e69949338
Michael de Silva <mike@stembolt.com>
2015-10-07 02:58:23 +0530

    Add usage output to STDOUT for playing script

--- koans/greed/play_greed.rb
+++ koans/greed/play_greed.rb
@@ -1,4 +1,14 @@
 #!/usr/bin/env ruby
 require File.expand_path(File.dirname(__FILE__) + "/lib/greed")
 
+if ARGV.empty?
+  STDOUT.puts <<-EOF
+Please provide player names
+
+Usage:
+  play_greed [NAMES]
+EOF
+  abort
+end
+
 Greed::Game.start_game(*ARGV)
